27205c1a511f5b832c1116a31a2a9714a2958647
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-26 15:19:14 +0800

    Started game class

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        
+        self.screen_width, self.screen_height = 1280, 720
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("BlackJack")
+        
+    def get_font(self, size):
+        return pygame.font.Font(None, size)
+
+    def run(self):
+        while self.running:
+            self.screen.fill((202, 228, 241))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            pygame.display.update()
+        pygame.quit()
+    
+Game.run()
+    
