d886a854bdb35fe1eaf7598fa2aa39ce7e56166c
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-26 15:51:12 +0800

    add way to load in deck of cards and assign its values

--- deck.py
+++ deck.py
@@ -11,4 +11,11 @@ class Deck:
         self.cards =[]
         self.load_cards()
         
-    
+    def load_cards(self):
+        self.cards.clear()
+        for file in self.card_files:
+            image = pygame.image.load(file).convert_alpha()
+            image = pygame.transform.scale(image, (128, 192))
+            rank = int(file.split()[1].split('.')[0])
+            value = 11 if rank == 1 else 10 if rank >= 11 else rank
+            self.cards.append({"image": image, "value": value})
