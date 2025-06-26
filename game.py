8e2ba1947cf74912708cd4ae410ca4e6dd637809
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-26 16:44:14 +0800

    i mistakenly named the object a different name

--- game.py
+++ game.py
@@ -28,7 +28,7 @@ class Game:
     def get_font(self, size):
         return pygame.font.Font(None, size)
 
-    def get_buttons(self):
+    def load_buttons(self):
         self.start_button = Button(320, 500, pygame.image.load("assets/buttons/start_btn.png"), 1)
         self.exit_button = Button(670, 500, pygame.image.load("assets/buttons/exit.png"), 1)
         self.hit_button = Button(300, 600, pygame.image.load("assets/buttons/hit.png"), 1)
