a0fc88e85b8b236e22d61a15b7c2c9d2219dd2e7
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-26 15:58:14 +0800

    we then add the values of the cards dealt. it is important that the aces are to be stored as either 1 or 11 depending on the value of the current hand.

--- hand.py
+++ hand.py
@@ -4,3 +4,11 @@ class hand:
         
     def add_card(self, card):
         self.cards.append (card)
+        
+    def total(self):
+        value = sum(card["value"] for card in self.cards)
+        aces = sum(1 for card in self.cards if card["value}" == 11])
+        while value > 21 and aces:
+            value -= 10
+            aces -= 1
+        return value    
