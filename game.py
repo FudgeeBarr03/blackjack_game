a0f1cc97ecf953172fc716e6b4bf64d6c5a2c755
FudgeeBarr03 <fudgeebar024@gmail.com>
2025-06-26 16:07:20 +0800

    initialize object values

--- game.py
+++ game.py
@@ -6,10 +6,24 @@ from button import Button
 class Game:
     def __init__(self):
         pygame.init()
+        pygame.font.init()
         
         self.screen_width, self.screen_height = 1280, 720
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("BlackJack")
+        
+        self.deck = Deck()
+        self.player = Hand()
+        self.dealer = Hand()
+        
+        self.running = True
+        self.game_over = False
+        self.player_in = True
+        self.dealer_in = True
+        
+        self.face_down_card = pygame.transform.scale(pygame.image.load("assets/cards/card back 3.png").convert_alpha(), (128, 192))
+        
+        self.load_buttons()
         
     def get_font(self, size):
         return pygame.font.Font(None, size)
